67de48a87bdbfc01be54938761e39d8a5e3e6266
Willem Deconinck <willem.deconinck@ecmwf.int>
2025-02-17 10:04:05 +0100

    Revert formatting for clarity

--- src/atlas-orca/util/OrcaData.cc
+++ src/atlas-orca/util/OrcaData.cc
@@ -17,9 +17,9 @@
 #include "atlas/io/atlas-io.h"
 namespace eckit::codec {
 using RecordWriter = atlas::io::RecordWriter;
-using atlas::io::ArrayReference;
 using atlas::io::ref;
-}  // namespace eckit::codec
+using atlas::io::ArrayReference;
+}
 #endif
 
 #include "eckit/log/ProgressTimer.h"
